2d35ffd7fda491b2d074503c710dc21d71441f90
Sean Blessing <snblessing@gmail.com>
2021-03-15 16:28:52 -0400

    init commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            int n1 = 6;
+            int n2 = 7;
+            
+            Console.WriteLine(n1 + n2);
+
+            Console.Write("Enter a whole number: ");
+            int n3 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter first name: ");
+            string fName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            String lName = Console.ReadLine();
+            String fullName = fName + lName;
+            Console.WriteLine(fullName);
+
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.Write("Enter color name: (r/g/b) ");
+                String color = Console.ReadLine();
+                if (color=="r") {
+                    Console.WriteLine("Red was picked!");
+                }
+                else if (color == "g") {
+                    Console.WriteLine("Green was picked!");
+                }
+                else if (color == "b") {
+                    Console.WriteLine("Blue was picked!");
+                }
+                Console.WriteLine("Continue (y/n)? ");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ MethodsApp/MethodsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodsApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the methods app!");
+
+            int nbr = GetInt("Enter a whole number: ");
+
+            Console.WriteLine("# entered: " + nbr);
+
+            Console.WriteLine("Bye!");
+        }
+
+        private static int GetInt(String prompt) {
+            int nbr = 0;
+            Boolean success = false;
+            while (!success) {
+                Console.Write(prompt);
+                try {
+                    nbr = Int32.Parse(Console.ReadLine());
+                    success = true;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Incorrect whole number.  Try again.");
+                }
+            }
+            return nbr;
+        }
+    }
+}
