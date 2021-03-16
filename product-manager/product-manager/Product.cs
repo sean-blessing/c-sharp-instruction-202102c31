23a4c71c0b3f13f1edbc7b2bba050e7baa8a0a5c
Sean Blessing <snblessing@gmail.com>
2021-03-16 10:35:33 -0400

    ch7 creating classes

---
+++ product-manager/product-manager/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace product_manager {
+    class Product {
+        public String Code { get; set; }
+        public String Description { get; set; }
+        public double Price { get; set; }
+
+        public Product() {
+
+        }
+        public Product(String code, String desc, double price) {
+            this.Code = code;
+            this.Description = desc;
+            this.Price = price;
+
+        }
+
+
+        public override string ToString() {
+            return $"Product: {Code}, {Description}, {Price.ToString("C")}";
+        }
+    }
+}
---
+++ product-manager/product-manager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace product_manager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Product Manager!!");
+
+            Console.Write("Enter product code: ");
+            String code = Console.ReadLine();
+            Console.Write("Enter product description: ");
+            String desc = Console.ReadLine();
+            Console.Write("Enter product price: ");
+            String priceStr = Console.ReadLine();
+            double price = Convert.ToDouble(priceStr);
+            Product p1 = new Product(code, desc, price);
+
+            //Console.WriteLine("Product entered: " + code +
+            //                    ", " + desc + ", " + price);
+            // string interpolation
+            //Console.WriteLine($"Product entered: {code},  {desc} ,  {price}");
+            Console.WriteLine(p1);
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
